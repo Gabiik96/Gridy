5ab54c553360baa7b3ffaf269b72ddc37b4e048c
Gabriel Balta <g.balta96@gmail.com>
2020-03-31 23:36:41 +0100

    Added Custom ImageView Cocoa class file

---
+++ Gridy/Model/CustomImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomView.swift
+//  Gridy
+//
+//  Created by Gabriel Balta on 31/03/2020.
+//  Copyright © 2020 Gabriel Balta. All rights reserved.
+//
+
+import UIKit
+
+class CustomView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
